efffac779bc5f557a7ce0ea1606a6a7e68885211
Brian Wang <brianwang7102@gmail.com>
2026-02-26 01:24:50 -0500

    Completed footer

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,38 +1,83 @@
+import { useEffect, useRef, type RefObject } from "react";
 import Logo from "../logo/logo";
 import styles from "./header.module.css";
 
-const Header: React.FC<{}> = () => {
+type HeaderProps = {
+    includeBuffer: boolean;
+};
+
+const Header: React.FC<HeaderProps> = (props) => {
+    const navBarRef = useRef<HTMLDivElement>(null) as RefObject<HTMLDivElement>;
+
+    useEffect(() => {
+        const visibilityThresholdElement =
+            document.querySelector("#thresholdElement");
+
+        const visibilityThreshold = (() => {
+            if (visibilityThresholdElement) {
+                const { bottom } =
+                    visibilityThresholdElement.getBoundingClientRect();
+                return (
+                    bottom -
+                    navBarRef.current.getBoundingClientRect().height -
+                    40
+                );
+            } else {
+                return 120;
+            }
+        })();
+
+        const onScroll = () => {
+            if (window.scrollY > visibilityThreshold) {
+                navBarRef.current.classList.add(styles.scrolled);
+            } else {
+                navBarRef.current.classList.remove(styles.scrolled);
+            }
+        };
+
+        window.addEventListener("scroll", onScroll);
+        return () => {
+            window.removeEventListener("scroll", onScroll);
+        };
+    }, []);
+
     return (
-        <header>
-            <div className={styles.headerTop}>
-                <Logo
-                    containerStyle={""}
-                    logoStyle={""}
-                    titleStyle={""}
-                    subtitleStyle={""}
-                />
-                <nav>
-                    <a href="/">Home</a>
-                    <a href="/locations">Our Locations</a>
-                    <a href="/team">Our team</a>
-                    <a href="/eye-surgury">Eye Surgeries</a>
-                    <a href="/eye-exam">Eye Exams</a>
-                    <a href="/resources">Resources</a>
-                </nav>
-            </div>
-            <div className={styles.headerRibbon}>
-                <div className={styles.langSelect}>English | 中文</div>
-                <div className={styles.infoRibbon}>
-                    <span>
-                        <span className={styles.fontBold}>Manhatten </span>|
-                        (212) 219-7786
-                    </span>
-                    <span>
-                        <span className={styles.fontBold}>Brooklyn </span>|
-                        (718) 492-3500
-                    </span>
+        <header className={styles.header}>
+            <div className={styles.content}>
+                <div className={styles.navbar} ref={navBarRef}>
+                    <Logo
+                        containerStyle={""}
+                        logoStyle={""}
+                        titleStyle={""}
+                        subtitleStyle={""}
+                    />
+                    <nav>
+                        <a href="/">Home</a>
+                        <a href="/locations">Our Locations</a>
+                        <a href="/team">Our team</a>
+                        <a href="/eye-surgury">Eye Surgeries</a>
+                        <a href="/eye-exam">Eye Exams</a>
+                        <a href="/resources">Resources</a>
+                    </nav>
+                </div>
+                <div className={styles.headerRibbon}>
+                    <div className={styles.langSelect}>English | 中文</div>
+                    <div className={styles.infoRibbon}>
+                        <span>
+                            <span className={styles.fontBold}>Manhatten </span>|
+                            (212) 219-7786
+                        </span>
+                        <span>
+                            <span className={styles.fontBold}>Brooklyn </span>|
+                            (718) 492-3500
+                        </span>
+                    </div>
                 </div>
             </div>
+            <div
+                className={styles.headerBuffer}
+                hidden={props.includeBuffer == false}
+            ></div>
         </header>
     );
 };
--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,17 +1,17 @@
 import styles from "./logo.module.css";
 
 type LogoProps = {
-    containerStyle: string;
-    logoStyle: string;
-    titleStyle: string;
-    subtitleStyle: string;
+    containerStyle?: string;
+    logoStyle?: string;
+    titleStyle?: string;
+    subtitleStyle?: string;
 };
 
 const Logo: React.FC<LogoProps> = (props) => {
-    const containerStyle = `${props.containerStyle} ${styles.container}`;
-    const logoStyle = `${props.logoStyle} ${styles.logo}`;
-    const titleStyle = [props.titleStyle, styles.title].join(" ");
-    const subtitleStyle = [props.subtitleStyle, styles.subtitle].join(" ");
+    const containerStyle = `${styles.container} ${props.containerStyle} `;
+    const logoStyle = `${styles.logo} ${props.logoStyle}`;
+    const titleStyle = `${styles.title} ${props.titleStyle}`;
+    const subtitleStyle = `${styles.subtitle} ${props.subtitleStyle}`;
     return (
         <div className={containerStyle}>
             <img src="/favicon.svg" className={logoStyle} />
