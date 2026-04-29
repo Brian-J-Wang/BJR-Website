3b8fe5ac5972e7ad008a4b545a36098eb9444e8f
Brian Wang <brianwang7102@gmail.com>
2026-04-29 01:46:20 -0400

    more work to make the website more clinical

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,26 +1,26 @@
 import styles from "./logo.module.css";
 
 type LogoProps = {
-    containerStyle?: string;
-    logoStyle?: string;
-    titleStyle?: string;
-    subtitleStyle?: string;
+	containerStyle?: string;
+	logoStyle?: string;
+	titleStyle?: string;
+	subtitleStyle?: string;
 };
 
 const Logo: React.FC<LogoProps> = (props) => {
-    const containerStyle = `${styles.container} ${props.containerStyle} `;
-    const logoStyle = `${styles.logo} ${props.logoStyle}`;
-    const titleStyle = `${styles.title} ${props.titleStyle}`;
-    const subtitleStyle = `${styles.subtitle} ${props.subtitleStyle}`;
-    return (
-        <div className={containerStyle}>
-            <img src="/favicon.svg" className={logoStyle} />
-            <div>
-                <h1 className={titleStyle}>BingJing Roberts</h1>
-                <small className={subtitleStyle}>Ophthalmology</small>
-            </div>
-        </div>
-    );
+	const containerStyle = `${styles.container} ${props.containerStyle} `;
+	const logoStyle = `${styles.logo} ${props.logoStyle}`;
+	const titleStyle = `${styles.title} ${props.titleStyle}`;
+	const subtitleStyle = `${styles.subtitle} ${props.subtitleStyle}`;
+	return (
+		<div className={containerStyle}>
+			<img src="/favicon.svg" className={logoStyle} alt="BJR Logo" />
+			<div className={styles.textContainer}>
+				<h1 className={titleStyle}>BINGJING ROBERTS</h1>
+				<span className={subtitleStyle}>OPHTHALMOLOGY</span>
+			</div>
+		</div>
+	);
 };
 
 export default Logo;
