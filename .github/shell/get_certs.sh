15a6c66c00c0a61707d7dbabd0a1863944e5fd4e
Brian <brianwang7102@gmail.com>
2026-07-17 22:41:37 -0400

    updated file

---
+++ .github/shell/get_certs.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+if [ ! -f /etc/letsencrypt/live/$DOMAIN/fullchain.pem ]; then
+    sudo certbot certonly --webroot \
+        --non-interactive --agree-tos \
+        --no-eff-email --email ${EMAIL}
+        -w /home/${USER}/certbot/www \
+        -d ${DOMAIN} 
+else
+    sudo certbot renew
+fi
+
